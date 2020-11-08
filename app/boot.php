54f5ceedd4907d7dcb60a11a1d9693ea24f61104
mikka <ezsolt@gmail.com>
2020-11-08 10:31:24 +0100

    simple ui for debugging

--- app/boot.php
+++ app/boot.php
@@ -4,7 +4,40 @@
  */
 declare(strict_types = 1);
 
+$payload = [
+    'status' => '42',
+    'message' => 'hello heroku world ? php file from github served through heroturko'
+];
+
+header('Content-Type: text/html; charset=utf-8');
+
+$layout = include __DIR__ . '/template/layout.php';
+
+exit;
 
 
-echo '<pre>';
-echo 'hello';
+
+/*
+use Atlas\Pdo\Connection;
+
+$connection = Connection::new(
+    'pgsql:host=localhost;dbname=',
+    '',
+    ''
+);
+
+$stm  = 'SELECT * FROM project WHERE published = :published';
+$bind = ['published' => 1];
+$mth = $connection->prepare($stm);
+$mth->execute($bind);
+$result = $mth->fetchAll(PDO::FETCH_ASSOC);
+
+print_r($result);
+
+echo 'YIELDING';
+echo '<hr>';
+foreach ($connection->yieldAll($stm, $bind) as $row) {
+    print_r($row);
+}
+echo '<hr>';
+*/
---
+++ app/template/layout.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <title>elevenone.space</title>
+    <link rel="stylesheet" href="/ui/css/main.css" />
+    <link rel="icon" href="/ui/favicon.png" />
+</head>
+<body>
+<div id="header">
+<pre>
+<code>
+<?php print_r ($payload); ?>
+</code>
+</pre>
+</div>
+</body>
+</html>
